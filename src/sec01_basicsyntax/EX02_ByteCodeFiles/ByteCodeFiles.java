ddf7603884fc646c182ec9fe6ee46617ea0f77db
Jeon Seungyeon <jsy020222@gmail.com>
2022-04-12 09:57:50 +0900

    파일 생성

---
+++ src/sec01_basicsyntax/EX02_ByteCodeFiles/ByteCodeFiles.java
@@ -0,0 +1,25 @@
+package sec01_basicsyntax.EX02_ByteCodeFiles;
+
+/*컴파일 이후 생성되는 실행파일(.class)*/
+
+class A { //A.class
+	
+}
+
+class B { //B.class
+	
+}
+
+class C { //C.class
+	class D { //C$D.class
+		
+	}
+}
+
+public class ByteCodeFiles {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
